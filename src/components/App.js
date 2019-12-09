44b7547ddd204101516f18c6d03ccf851e5bd486
Samuel P. Gillispie II <Samuel.Gillispie@ibm.com>
2019-12-08 20:38:45 -0600

    Working on button links

--- src/components/App.js
+++ src/components/App.js
@@ -9,6 +9,7 @@ const LogoImage = styled.img.attrs(props => ({ src: logoImageUrl }))`
 
 const HeroText = styled.h1`
   margin-top: 0;
+  margin-bottom: 1rem;
 
   font-family: "Roboto Slab", serif;
   font-size: 96pt;
@@ -16,11 +17,36 @@ const HeroText = styled.h1`
   letter-spacing: 25px;
 `;
 
+const ButtonRow = styled.div`
+  > *:not(:last-child) {
+    margin-right: 1rem;
+  }
+`;
+
+const Button = styled.button`
+  border: none;
+  border-radius: 0.5rem;
+  padding: 1rem;
+
+  background: #7289da;
+
+  font-size: 20pt;
+  color: #ffffff;
+`;
+
+const DiscordButton = styled(Button)``;
+
+const DestinyButton = styled(Button)``;
+
 export default function App() {
   return (
     <>
       <LogoImage />
       <HeroText>Exo/Terra</HeroText>
+      <ButtonRow>
+        <DiscordButton>Join us on Discord</DiscordButton>
+        <DestinyButton>Join our clan</DestinyButton>
+      </ButtonRow>
     </>
   );
 }
